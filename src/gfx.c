7ea82797c8e4ecaed0cd9c1192121f7ee74ca524
doremi <152659804+doremiposting@users.noreply.github.com>
2026-01-03 23:12:50 -0500

    rainbow triangle pt2

--- src/gfx.c
+++ src/gfx.c
@@ -38,9 +38,7 @@ tri2duv(Tri2d t, int x, int y) {
   g = (int)(0xFF - (d2 * 0xFF / maxdist));
   b = (int)(0xFF - (d3 * 0xFF / maxdist));
 
-  r = (r < 0) ? 0 : (r > 255) ? 255 : r;
-  g = (g < 0) ? 0 : (g > 255) ? 255 : g;
-  b = (b < 0) ? 0 : (b > 255) ? 255 : b;
+  r %= 0xFF; g %= 0xFF; b %= 0xFF;
 
   color =  ((a << 24) | (r << 16) | (g << 8) | (b));
   return color;
